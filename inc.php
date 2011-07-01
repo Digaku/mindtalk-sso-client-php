5539a63cfbd18ddb972e6452e66ff93cc4508628
robin <obin.mf@gmail.com>
2011-07-02 02:04:33 +0700

    add helper url_get_data

--- inc.php
+++ inc.php
@@ -3,12 +3,33 @@
  * Copyright (C) 2011 Ansvia.
  * License:			MIT
  * File:           inc.php
- * Summary:        Digaku SSO engine example.
- * First writter:  robin <robin [at] digaku [dot] kom> 
+ * Summary:        Helper.
+ * First writter:  robin <robin [at] digaku [dot] kom>
  */
 
 require_once("config.php");
 
+
+/**
+ * function url_get_data
+ * Helper untuk url GET.
+ * Get data from given url.
+ * @param {String} $url -- url to download.
+ */
+function url_get_data($url)
+{
+	$ch = curl_init($url);
+	
+	curl_setopt( $ch, CURLOPT_HEADER, false );
+	curl_setopt( $ch, CURLOPT_RETURNTRANSFER, true );
+	curl_setopt( $ch, CURLOPT_FOLLOWLOCATION, true );
+	
+	$result = curl_exec( $ch );
+	
+    curl_close( $ch );
+    
+	return $result;
+}
 
 /**
  * Helpers untuk ngepost data.
